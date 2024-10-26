21ea599acc512a02c574cc75351a149e82fc199a
Akshay Mestry <xa@mes3.dev>
2024-10-25 22:47:50 -0500

    docs: upstream test documentation
    Signed-off-by: Akshay Mestry <xa@mes3.dev>

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Void'
+copyright = '2024, Akshay Mestry'
+author = 'Akshay Mestry'
+release = '2024.11.01'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'coeus_sphinx_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,15 @@
+.. The Void documentation master file, created by
+   sphinx-quickstart on Fri Oct 25 22:40:32 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+The Void documentation
+======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+We're testing the website and please expect a lot of bugs!
+
+This is going to be pushed on the GitHub Pages... see you there!
